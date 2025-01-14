91b12ab5ca65ba552fd2144543924512b37c3025
Daniel Tomé Fernández <danieltomefer@gmail.com>
2025-01-14 22:05:45 +0100

    Step 1 and 2 done

--- dao-governance-proposals-and-voting/packages/foundry/contracts/Governance.sol
+++ dao-governance-proposals-and-voting/packages/foundry/contracts/Governance.sol
@@ -1,4 +1,48 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-// Good luck!
+import { DecentralizedResistanceToken } from
+  "./DecentralizedResistanceToken.sol";
+
+contract Governance {
+  event ProposalCreated(uint proposalId, string title, uint votingDeadline, address creator);
+
+  DecentralizedResistanceToken token;
+  uint256 votingPeriod;
+
+  /**
+   * @dev Only one proposal can be active.
+   */
+  Proposal activeProposal;
+  /**
+   * @dev Only one proposal can be queued.
+   */
+  Proposal proposalQueue;
+  uint8 nextProposalId;
+
+
+  struct Proposal {
+    uint256 deadline;
+  }
+
+  constructor(address _tokenAddress, uint256 _votingPeriod) {
+    token = DecentralizedResistanceToken(_tokenAddress);
+    votingPeriod = _votingPeriod;
+    nextProposalId = 0;
+  }
+
+  /**
+   * Function to create new proposal.
+   * @param _title Proposal title
+   * @return proposalId Returns the proposalId.
+   */
+  function propose(string memory _title) external returns(uint256) {
+    uint256 proposalId = nextProposalId;
+
+
+    emit ProposalCreated(proposalId, _title, votingPeriod, msg.sender);
+    nextProposalId++;
+
+    return proposalId;
+  }
+}
