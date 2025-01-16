66351a67314619e8ed3ef84f8812961d2d790755
Daniel Tomé Fernández <danieltomefer@gmail.com>
2025-01-16 23:01:00 +0100

    Finished multisend challenge

--- multisend/packages/foundry/contracts/Multisend.sol
+++ multisend/packages/foundry/contracts/Multisend.sol
@@ -2,5 +2,86 @@
 pragma solidity 0.8.26;
 
 import { console2 } from "forge-std/console2.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-// Good luck!
+contract Multisend {
+  ////////////////
+  ////  Errors
+  ////////////////
+  error Multisend__NotValidLengths();
+  error Multisend__ETHTransferFailed(address receiver, uint256 amount);
+
+  ////////////////
+  ////  Events
+  ////////////////
+  event SuccessfulETHTransfer(
+    address _sender, address payable[] _receivers, uint256[] _amounts
+  );
+  event SuccessfulTokenTransfer(
+    address indexed _sender,
+    address[] indexed _receivers,
+    uint256[] _amounts,
+    address _token
+  );
+
+  constructor() { }
+
+  /////////////////////////
+  ////  External functions
+  /////////////////////////
+  function sendETH(
+    address payable[] calldata receivers,
+    uint256[] calldata amounts
+  ) external payable {
+    if (receivers.length != amounts.length) {
+      revert Multisend__NotValidLengths();
+    }
+    _sentETHToAllReceivers(receivers, amounts);
+    emit SuccessfulETHTransfer(msg.sender, receivers, amounts);
+  }
+
+  function sendTokens(
+    address[] calldata receivers,
+    uint256[] calldata amounts,
+    address token
+  ) external {
+    if (receivers.length != amounts.length) {
+      revert Multisend__NotValidLengths();
+    }
+    _sendTokensToAllReceivers(receivers, amounts, token);
+    emit SuccessfulTokenTransfer(msg.sender, receivers, amounts, token);
+  }
+
+  receive() external payable { }
+
+  /////////////////////////
+  ////  Private Functions
+  /////////////////////////
+  function _sentETHToAllReceivers(
+    address payable[] calldata receivers,
+    uint256[] calldata amounts
+  ) private {
+    uint256 loopLength = receivers.length;
+    for (uint256 i = 0; i < loopLength; i++) {
+      (bool success,) = (receivers[i]).call{ value: amounts[i] }("");
+      if (!success) {
+        revert Multisend__ETHTransferFailed(receivers[i], amounts[i]);
+      }
+    }
+  }
+
+  function _sendTokensToAllReceivers(
+    address[] calldata receivers,
+    uint256[] calldata amounts,
+    address tokenAddr
+  ) private {
+    uint256 loopLength = receivers.length;
+    IERC20 token = IERC20(tokenAddr);
+    for (uint256 i = 0; i < loopLength; i++) {
+      bool success = token.transferFrom(msg.sender, receivers[i], amounts[i]);
+      if (!success) {
+        revert Multisend__ETHTransferFailed(receivers[i], amounts[i]);
+      }
+    }
+  }
+}
--- multisend/packages/nextjs/contracts/deployedContracts.ts
+++ multisend/packages/nextjs/contracts/deployedContracts.ts
@@ -4,6 +4,485 @@
  */
 import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 
-const deployedContracts = {} as const;
+const deployedContracts = {
+  11155111: {
+    MockToken: {
+      address: "0xb0f4cd5441148fbda4d6adab825039cb53ae63ba",
+      abi: [
+        {
+          type: "constructor",
+          inputs: [
+            {
+              name: "name",
+              type: "string",
+              internalType: "string",
+            },
+            {
+              name: "symbol",
+              type: "string",
+              internalType: "string",
+            },
+          ],
+          stateMutability: "nonpayable",
+        },
+        {
+          type: "function",
+          name: "allowance",
+          inputs: [
+            {
+              name: "owner",
+              type: "address",
+              internalType: "address",
+            },
+            {
+              name: "spender",
+              type: "address",
+              internalType: "address",
+            },
+          ],
+          outputs: [
+            {
+              name: "",
+              type: "uint256",
+              internalType: "uint256",
+            },
+          ],
+          stateMutability: "view",
+        },
+        {
+          type: "function",
+          name: "approve",
+          inputs: [
+            {
+              name: "spender",
+              type: "address",
+              internalType: "address",
+            },
+            {
+              name: "value",
+              type: "uint256",
+              internalType: "uint256",
+            },
+          ],
+          outputs: [
+            {
+              name: "",
+              type: "bool",
+              internalType: "bool",
+            },
+          ],
+          stateMutability: "nonpayable",
+        },
+        {
+          type: "function",
+          name: "balanceOf",
+          inputs: [
+            {
+              name: "account",
+              type: "address",
+              internalType: "address",
+            },
+          ],
+          outputs: [
+            {
+              name: "",
+              type: "uint256",
+              internalType: "uint256",
+            },
+          ],
+          stateMutability: "view",
+        },
+        {
+          type: "function",
+          name: "decimals",
+          inputs: [],
+          outputs: [
+            {
+              name: "",
+              type: "uint8",
+              internalType: "uint8",
+            },
+          ],
+          stateMutability: "view",
+        },
+        {
+          type: "function",
+          name: "mint",
+          inputs: [
+            {
+              name: "amount",
+              type: "uint256",
+              internalType: "uint256",
+            },
+          ],
+          outputs: [],
+          stateMutability: "nonpayable",
+        },
+        {
+          type: "function",
+          name: "name",
+          inputs: [],
+          outputs: [
+            {
+              name: "",
+              type: "string",
+              internalType: "string",
+            },
+          ],
+          stateMutability: "view",
+        },
+        {
+          type: "function",
+          name: "symbol",
+          inputs: [],
+          outputs: [
+            {
+              name: "",
+              type: "string",
+              internalType: "string",
+            },
+          ],
+          stateMutability: "view",
+        },
+        {
+          type: "function",
+          name: "totalSupply",
+          inputs: [],
+          outputs: [
+            {
+              name: "",
+              type: "uint256",
+              internalType: "uint256",
+            },
+          ],
+          stateMutability: "view",
+        },
+        {
+          type: "function",
+          name: "transfer",
+          inputs: [
+            {
+              name: "to",
+              type: "address",
+              internalType: "address",
+            },
+            {
+              name: "value",
+              type: "uint256",
+              internalType: "uint256",
+            },
+          ],
+          outputs: [
+            {
+              name: "",
+              type: "bool",
+              internalType: "bool",
+            },
+          ],
+          stateMutability: "nonpayable",
+        },
+        {
+          type: "function",
+          name: "transferFrom",
+          inputs: [
+            {
+              name: "from",
+              type: "address",
+              internalType: "address",
+            },
+            {
+              name: "to",
+              type: "address",
+              internalType: "address",
+            },
+            {
+              name: "value",
+              type: "uint256",
+              internalType: "uint256",
+            },
+          ],
+          outputs: [
+            {
+              name: "",
+              type: "bool",
+              internalType: "bool",
+            },
+          ],
+          stateMutability: "nonpayable",
+        },
+        {
+          type: "event",
+          name: "Approval",
+          inputs: [
+            {
+              name: "owner",
+              type: "address",
+              indexed: true,
+              internalType: "address",
+            },
+            {
+              name: "spender",
+              type: "address",
+              indexed: true,
+              internalType: "address",
+            },
+            {
+              name: "value",
+              type: "uint256",
+              indexed: false,
+              internalType: "uint256",
+            },
+          ],
+          anonymous: false,
+        },
+        {
+          type: "event",
+          name: "Transfer",
+          inputs: [
+            {
+              name: "from",
+              type: "address",
+              indexed: true,
+              internalType: "address",
+            },
+            {
+              name: "to",
+              type: "address",
+              indexed: true,
+              internalType: "address",
+            },
+            {
+              name: "value",
+              type: "uint256",
+              indexed: false,
+              internalType: "uint256",
+            },
+          ],
+          anonymous: false,
+        },
+        {
+          type: "error",
+          name: "ERC20InsufficientAllowance",
+          inputs: [
+            {
+              name: "spender",
+              type: "address",
+              internalType: "address",
+            },
+            {
+              name: "allowance",
+              type: "uint256",
+              internalType: "uint256",
+            },
+            {
+              name: "needed",
+              type: "uint256",
+              internalType: "uint256",
+            },
+          ],
+        },
+        {
+          type: "error",
+          name: "ERC20InsufficientBalance",
+          inputs: [
+            {
+              name: "sender",
+              type: "address",
+              internalType: "address",
+            },
+            {
+              name: "balance",
+              type: "uint256",
+              internalType: "uint256",
+            },
+            {
+              name: "needed",
+              type: "uint256",
+              internalType: "uint256",
+            },
+          ],
+        },
+        {
+          type: "error",
+          name: "ERC20InvalidApprover",
+          inputs: [
+            {
+              name: "approver",
+              type: "address",
+              internalType: "address",
+            },
+          ],
+        },
+        {
+          type: "error",
+          name: "ERC20InvalidReceiver",
+          inputs: [
+            {
+              name: "receiver",
+              type: "address",
+              internalType: "address",
+            },
+          ],
+        },
+        {
+          type: "error",
+          name: "ERC20InvalidSender",
+          inputs: [
+            {
+              name: "sender",
+              type: "address",
+              internalType: "address",
+            },
+          ],
+        },
+        {
+          type: "error",
+          name: "ERC20InvalidSpender",
+          inputs: [
+            {
+              name: "spender",
+              type: "address",
+              internalType: "address",
+            },
+          ],
+        },
+      ],
+      inheritedFunctions: {},
+    },
+    Multisend: {
+      address: "0x6d5cecf51f4feb724e9352a63568b5f7542c41fb",
+      abi: [
+        {
+          type: "constructor",
+          inputs: [],
+          stateMutability: "nonpayable",
+        },
+        {
+          type: "receive",
+          stateMutability: "payable",
+        },
+        {
+          type: "function",
+          name: "sendETH",
+          inputs: [
+            {
+              name: "receivers",
+              type: "address[]",
+              internalType: "address payable[]",
+            },
+            {
+              name: "amounts",
+              type: "uint256[]",
+              internalType: "uint256[]",
+            },
+          ],
+          outputs: [],
+          stateMutability: "payable",
+        },
+        {
+          type: "function",
+          name: "sendTokens",
+          inputs: [
+            {
+              name: "receivers",
+              type: "address[]",
+              internalType: "address[]",
+            },
+            {
+              name: "amounts",
+              type: "uint256[]",
+              internalType: "uint256[]",
+            },
+            {
+              name: "token",
+              type: "address",
+              internalType: "address",
+            },
+          ],
+          outputs: [],
+          stateMutability: "nonpayable",
+        },
+        {
+          type: "event",
+          name: "SuccessfulETHTransfer",
+          inputs: [
+            {
+              name: "_sender",
+              type: "address",
+              indexed: false,
+              internalType: "address",
+            },
+            {
+              name: "_receivers",
+              type: "address[]",
+              indexed: false,
+              internalType: "address payable[]",
+            },
+            {
+              name: "_amounts",
+              type: "uint256[]",
+              indexed: false,
+              internalType: "uint256[]",
+            },
+          ],
+          anonymous: false,
+        },
+        {
+          type: "event",
+          name: "SuccessfulTokenTransfer",
+          inputs: [
+            {
+              name: "_sender",
+              type: "address",
+              indexed: true,
+              internalType: "address",
+            },
+            {
+              name: "_receivers",
+              type: "address[]",
+              indexed: true,
+              internalType: "address[]",
+            },
+            {
+              name: "_amounts",
+              type: "uint256[]",
+              indexed: false,
+              internalType: "uint256[]",
+            },
+            {
+              name: "_token",
+              type: "address",
+              indexed: false,
+              internalType: "address",
+            },
+          ],
+          anonymous: false,
+        },
+        {
+          type: "error",
+          name: "Multisend__ETHTransferFailed",
+          inputs: [
+            {
+              name: "receiver",
+              type: "address",
+              internalType: "address",
+            },
+            {
+              name: "amount",
+              type: "uint256",
+              internalType: "uint256",
+            },
+          ],
+        },
+        {
+          type: "error",
+          name: "Multisend__NotValidLengths",
+          inputs: [],
+        },
+      ],
+      inheritedFunctions: {},
+    },
+  },
+} as const;
 
 export default deployedContracts satisfies GenericContractsDeclaration;
