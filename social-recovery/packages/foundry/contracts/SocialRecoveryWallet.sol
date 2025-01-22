f3c7833434f1cd3e63a976eb44dc8528c128a64c
Daniel Tomé Fernández <danieltomefer@gmail.com>
2025-01-22 09:33:06 +0100

    Finished challenge :)

--- social-recovery/packages/foundry/contracts/SocialRecoveryWallet.sol
+++ social-recovery/packages/foundry/contracts/SocialRecoveryWallet.sol
@@ -4,4 +4,97 @@ pragma solidity 0.8.26;
 
 import { console2 } from "forge-std/console2.sol";
 
-// Good luck!
+contract SocialRecoveryWallet {
+  /////////////////
+  //// STRUCTS ////
+  /////////////////
+
+  //////////////////
+  ///// Events /////
+  //////////////////
+  event NewOwnerSignaled(address by, address proposedOwner);
+  event RecoveryExecuted(address newOwner);
+
+  //////////////////////
+  ///// STATE VARS /////
+  //////////////////////
+  address public owner;
+  uint256 totalGuardians;
+  mapping(address guardian => bool isGuardian) guardians;
+  mapping(address proposedGuardian => mapping(address guardian => bool signal))
+    hasGuardianSignaled;
+  mapping(address proposedGuardian => uint256 guardiansSignaled)
+    numGuardiansSignaled;
+
+  receive() external payable { }
+
+  fallback() external { }
+
+  constructor(
+    address[] memory _guardians
+  ) {
+    owner = msg.sender;
+    for (uint256 i = 0; i < _guardians.length; i++) {
+      guardians[_guardians[i]] = true;
+    }
+    totalGuardians = _guardians.length;
+  }
+
+  //////////////////////////
+  ///// EXTERNAL FUNCS /////
+  //////////////////////////
+  function call(
+    address callee,
+    uint256 value,
+    bytes calldata data
+  ) external payable {
+    require(msg.sender == owner, "Only the owner can call this function");
+    (bool success,) = callee.call{ value: value }(data);
+    require(success, "Call failed");
+  }
+
+  function signalNewOwner(
+    address _proposedOwner
+  ) external {
+    require(guardians[msg.sender], "Only guardian");
+    require(
+      !hasGuardianSignaled[_proposedOwner][msg.sender], "Can only signal once"
+    );
+
+    hasGuardianSignaled[_proposedOwner][msg.sender] = true;
+    numGuardiansSignaled[_proposedOwner]++;
+    emit NewOwnerSignaled(msg.sender, _proposedOwner);
+
+    // all guardians have signaled.
+    if (totalGuardians == numGuardiansSignaled[_proposedOwner]) {
+      owner = _proposedOwner;
+      emit RecoveryExecuted(_proposedOwner);
+    }
+  }
+
+  function addGuardian(
+    address _guardian
+  ) external {
+    require(msg.sender == owner, "Only owner can add guardian");
+    require(_guardian != address(0), "Address must be different than zero");
+    require(!guardians[_guardian], "Only can be added once");
+    guardians[_guardian] = true;
+    totalGuardians++;
+  }
+
+  function removeGuardian(
+    address _guardian
+  ) external {
+    require(msg.sender == owner, "Only owner can add guardian");
+    require(_guardian != address(0), "Address must be different than zero");
+    require(guardians[_guardian], "Guardian not found");
+    guardians[_guardian] = false;
+    totalGuardians--;
+  }
+
+  function isGuardian(
+    address guardian
+  ) external view returns (bool) {
+    return guardians[guardian];
+  }
+}
--- social-recovery/packages/nextjs/contracts/deployedContracts.ts
+++ social-recovery/packages/nextjs/contracts/deployedContracts.ts
@@ -4,6 +4,160 @@
  */
 import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 
-const deployedContracts = {} as const;
+const deployedContracts = {
+  11155111: {
+    SocialRecoveryWallet: {
+      address: "0x080b89c411cfb9dc0d4aaef012cf8372aac305ca",
+      abi: [
+        {
+          type: "constructor",
+          inputs: [
+            {
+              name: "_guardians",
+              type: "address[]",
+              internalType: "address[]",
+            },
+          ],
+          stateMutability: "nonpayable",
+        },
+        {
+          type: "fallback",
+          stateMutability: "nonpayable",
+        },
+        {
+          type: "receive",
+          stateMutability: "payable",
+        },
+        {
+          type: "function",
+          name: "addGuardian",
+          inputs: [
+            {
+              name: "_guardian",
+              type: "address",
+              internalType: "address",
+            },
+          ],
+          outputs: [],
+          stateMutability: "nonpayable",
+        },
+        {
+          type: "function",
+          name: "call",
+          inputs: [
+            {
+              name: "callee",
+              type: "address",
+              internalType: "address",
+            },
+            {
+              name: "value",
+              type: "uint256",
+              internalType: "uint256",
+            },
+            {
+              name: "data",
+              type: "bytes",
+              internalType: "bytes",
+            },
+          ],
+          outputs: [],
+          stateMutability: "payable",
+        },
+        {
+          type: "function",
+          name: "isGuardian",
+          inputs: [
+            {
+              name: "guardian",
+              type: "address",
+              internalType: "address",
+            },
+          ],
+          outputs: [
+            {
+              name: "",
+              type: "bool",
+              internalType: "bool",
+            },
+          ],
+          stateMutability: "view",
+        },
+        {
+          type: "function",
+          name: "owner",
+          inputs: [],
+          outputs: [
+            {
+              name: "",
+              type: "address",
+              internalType: "address",
+            },
+          ],
+          stateMutability: "view",
+        },
+        {
+          type: "function",
+          name: "removeGuardian",
+          inputs: [
+            {
+              name: "_guardian",
+              type: "address",
+              internalType: "address",
+            },
+          ],
+          outputs: [],
+          stateMutability: "nonpayable",
+        },
+        {
+          type: "function",
+          name: "signalNewOwner",
+          inputs: [
+            {
+              name: "_proposedOwner",
+              type: "address",
+              internalType: "address",
+            },
+          ],
+          outputs: [],
+          stateMutability: "nonpayable",
+        },
+        {
+          type: "event",
+          name: "NewOwnerSignaled",
+          inputs: [
+            {
+              name: "by",
+              type: "address",
+              indexed: false,
+              internalType: "address",
+            },
+            {
+              name: "proposedOwner",
+              type: "address",
+              indexed: false,
+              internalType: "address",
+            },
+          ],
+          anonymous: false,
+        },
+        {
+          type: "event",
+          name: "RecoveryExecuted",
+          inputs: [
+            {
+              name: "newOwner",
+              type: "address",
+              indexed: false,
+              internalType: "address",
+            },
+          ],
+          anonymous: false,
+        },
+      ],
+      inheritedFunctions: {},
+    },
+  },
+} as const;
 
 export default deployedContracts satisfies GenericContractsDeclaration;
